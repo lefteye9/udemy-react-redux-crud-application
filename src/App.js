f845669a4c5d90110588b0d47aa528700fa5ab67
ryo <right_hand9@hotmail.com>
2020-10-07 02:15:32 +0900

    Learn-jsx-syntax

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          Hello World!
-        </a>
-      </header>
-    </div>
-  );
+//function App() {
+  //return (
+   //<div><h1>Hello World!</h1></div>
+  //);
+//}
+
+// class App extends Component{
+//   render(){
+//     const greeting = "Hi!TOM!";
+//   const dom = <h1 className="foo">{greeting}</h1>;
+//     return dom;
+//   }
+// }
+
+// class App extends Component{
+//   render(){
+//     return <input type="text" onChange={()=>{console.log("clicked");}}></input>
+//   }
+// }
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={()=>{console.log("clicked");}} />
+      </div>
+    )
+  }
 }
+
+// class App extends Component{
+//   render(){
+//     return React.createElement(
+//       "div",
+//       null,
+//       "Hello,World!!!"
+//     );
+//   }
+// }
 
 export default App;
