939f61e64455dd4a3c284ec3e2d50dfa3188194c
ryo <right_hand9@hotmail.com>
2020-11-23 15:10:01 +0900

    implement store

---
+++ src/components/App.js
@@ -0,0 +1,223 @@
+import React,{ Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+const App2 = () => (<Counter></Counter>)
+
+class Counter extends Component {
+  constructor(props){
+    super(props)
+    console.log(this.state);
+    this.state = {count: 0}
+  }
+
+  hundlePlusButton = () => {
+    console.log("hundlePlusButton")
+    console.log(this.state.count);
+    // const currentCount = this.state.count;
+    this.setState(
+      // {count: currentCount +1}
+      {count: this.state.count +1}
+    )
+  }
+
+  hundleMinusButton = () => {
+    console.log('Minus');
+    this.setState({count: this.state.count -1})
+  }
+
+
+
+  render(){
+    console.log(this.state);
+  return(
+  <React.Fragment>
+  <div>count:{this.state.count}</div>)
+  <button onClick={this.hundlePlusButton}>+1</button>
+  <button onClick={this.hundleMinusButton}>-1</button>
+  </React.Fragment>)
+  }
+}
+
+// const App2 = () => {
+//   return(
+//     <div>
+//       <User name={"Taro"} age={"10"}/>
+//       <User name={"Hanako"}/>
+//     </div>
+//   )
+// }
+
+// const App2 = () => {
+// const profiles = [
+//   {name:"Taro",age: 10},
+//   {name:"Hanako",age: 20},
+//   {name:"Himawari"}
+// ]
+
+// return(
+//   <div>
+//     {
+//       profiles.map((profile,index) => {
+//         console.log(profile); //profileは配列を順番に取り出したものが入る。
+//         console.log(index); //第二引数には配列の順番が自動的に入る。
+//         return <User name={profile.name} age={profile.age} key={index}/>
+//       })
+//     }
+//   </div>
+// )
+//   }
+
+// const User = (props) => {
+// return <h1>Hello,I'm {props.name},{props.age}years old!</h1>
+// }
+
+// User.defaultProps = {age:1};
+
+// User.PropTypes = {
+//   name:PropTypes.string,
+//   age:PropTypes.number.isRequired
+// }
+
+// const App2 = () =>{
+//   return (
+//     <div>
+//       <Cat />
+//       <Cat />
+//       <Cat />
+//     </div>
+//   )
+//   }
+
+//   const Cat = () =>{
+//     return(
+//       <div>Meow</div>
+//     )
+//   }
+
+// class App2 extends Component{
+//   render(){
+//   //   const greeting = "Hello World!";
+//   // const dom = <h1 className="foo">{greeting}</h1>;
+//   return(
+//   // <div>
+//   <React.Fragment>
+//   <label htmlFor="bar">bar</label>
+//   <input type="text" value="aaa" onClick={()=>{console.log("I'm clicked")}} />;
+//   {/* </div> */}
+//   </React.Fragment>)
+//   }
+// }
+
+export default App2;
+
+
+// const App = () => {
+
+//   const profiles = [
+//     {name:"Taro" ,age:10},
+//     {name:"Hanako" ,age:5}
+//   ]
+//   return(
+//   <div>
+//     {
+//     profiles.map( (profile) => {
+//       console.log(profile);
+//       return <User name={profile.name} age={profile.age} />
+//     })
+//   }
+//   </div>
+//   )
+// }
+
+const array1 = [1,3,5,7,9];
+console.log(array1);
+
+const array2 = array1.map( (num) => {
+  console.log(num);
+  return num*3;
+})
+console.log(array2);
+
+
+// // const string1 = "あああ";
+
+// // const string2 = string1.length( (num) => {
+// //   return num * 1111;
+// // })
+
+// // console.log(string2);
+
+// const array4 = [1,3,5,7,9];
+// const MyFunc = (num) => {return num*3};
+
+// const array5 = array4.map(MyFunc);
+
+// console.log(array5);
+
+
+
+
+
+
+
+{/* //   <div>
+//     <User name={"Taro"} age={10} />
+//     <User name={"Hanako"} age={5} />
+
+//     </div>
+//   )
+// } */}
+
+
+
+// const User = (props) =>{
+//   // console.log(props);
+// return <div>I"m {props.name}.{props.age}years old!</div>
+// };
+
+
+
+
+//function App() {
+  //return (
+   //<div><h1>Hello World!</h1></div>
+  //);
+//}
+
+// class App extends Component{
+//   render(){
+//     const greeting = "Hi!TOM!";
+//   const dom = <h1 className="foo">{greeting}</h1>;
+//     return dom;
+//   }
+// }
+
+// class App extends Component{
+//   render(){
+//     return <input type="text" onChange={()=>{console.log("clicked");}}></input>
+//   }
+// }
+
+// class App extends Component{
+//   render(){
+//     return(
+//       <div>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={()=>{console.log("clicked");}} />
+//       </div>
+//     )
+//   }
+// }
+
+// class App extends Component{
+//   render(){
+//     return React.createElement(
+//       "div",
+//       null,
+//       "Hello,World!!!"
+//     );
+//   }
+// }
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore} from 'redux'
+import { Provider} from 'react-redux'
+import reducer from '/reducers'
+
 import './index.css';
-import App from './App';
-import App2 from './App';
+import App from './components/App';
+import App2 from './components/App';
 
 import * as serviceWorker from './serviceWorker';
 
+//全てのstateはこのstoreに集約されている。
+const store = createStore(reducer)
+
+console.log(store);
+
+
+
 ReactDOM.render(
   <React.StrictMode>
+    {/* //providerコンポーネントでラップした後、storeにあるstateを渡す。 */}
+    <Provider store={store}>
     <App2 />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
